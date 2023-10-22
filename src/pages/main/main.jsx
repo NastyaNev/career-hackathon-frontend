6a7324d00cfb4de3bb96e0ddc254393f42daa147
Nastya N <asrozova@gmail.com>
2023-10-22 12:40:57 +0200

    feat:mui_add

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import styles from "./main.module.css"
+import styles from "./main.module.css";
+import Button from '@mui/material/Button';
 
 function Main() {
   return (
     <>
       <main className={styles.main_container}>
         <p className={styles.main_text}>Привет, я главная страница</p>
+        <Button variant="contained">Hello world</Button>;
       </main>
     </>
   )
