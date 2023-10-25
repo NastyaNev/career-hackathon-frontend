38f85a4b212e12f7a05a20ce3114900dce0ed3a7
Nastya N <asrozova@gmail.com>
2023-10-25 14:02:26 +0200

    publist_basename

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,15 @@ import "./index.css";
 import "./fonts/fonts.css";
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-      <HashRouter basename="/">
+      <BrowserRouter basename="/career-hackathon-frontend">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
 );
 
