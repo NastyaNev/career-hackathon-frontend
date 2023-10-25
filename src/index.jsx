b567567c019c3f7142163272690129c8c38898cd
Nastya N <asrozova@gmail.com>
2023-10-25 05:09:07 +0200

    resolv

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
-      <BrowserRouter basename="/career-hackathon-frontend">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </React.StrictMode>
