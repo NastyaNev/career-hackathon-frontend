b326590dd6cb4778e55be7470a2346afc3d10af2
Nastya N <asrozova@gmail.com>
2023-10-26 18:53:41 +0200

    slash

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
     <React.StrictMode>
-      <BrowserRouter basename="/career-hackathon-frontend/">
+      <BrowserRouter basename="/career-hackathon-frontend">
         <App />
       </BrowserRouter>
     </React.StrictMode>
