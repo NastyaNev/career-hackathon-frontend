d17de9e4a39f9eef83228ae72b3ffac359fa49b0
Nastya N <asrozova@gmail.com>
2023-10-26 04:02:08 +0200

    feat:alice

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -4,22 +4,28 @@ import hand from "../../images/hand.svg";
 import messages from "../../images/messages.svg";
 import IconButton from "@mui/material/Button";
 import { Stack, Tooltip } from "@mui/material";
+import search from '../../images/YaGPT.svg';
 
 function AppHeader() {
   return (
     <header className={styles.header_background}>
-      <Stack direction="row" useFlexGap justifyContent='flex-end' alignItems='center' className={styles.header_content}>
-        <Stack direction="row" spacing={1.5} mr={5}>
-          <Tooltip title="Уведомления">
-            <IconButton aria-label="delete" sx={{'&:hover': {opacity: [0.8, 0.8, 0.7]}, padding: 0, minWidth: 0}}>
-              <img src={hand} alt="уведомления" />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Сообщения">
-            <IconButton aria-label="delete" sx={{'&:hover': {opacity: [0.8, 0.8, 0.7]}, padding: 0, minWidth: 0}}>
-              <img src={messages} alt="сообщения" />
-            </IconButton>
-          </Tooltip>
+      <Stack direction="row" useFlexGap justifyContent='space-between' alignItems='center' className={styles.header_content}>
+        <Stack>
+            <img src={search} alt="поиск с Алисой" />
+        </Stack>
+        <Stack direction="row" useFlexGap justifyContent='flex-end' alignItems='center' className={styles.header_content}>
+          <Stack direction="row" spacing={1.5} mr={5}>
+            <Tooltip title="Уведомления">
+              <IconButton aria-label="delete" sx={{'&:hover': {opacity: [0.8, 0.8, 0.7]}, padding: 0, minWidth: 0}}>
+                <img src={hand} alt="уведомления" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Сообщения">
+              <IconButton aria-label="delete" sx={{'&:hover': {opacity: [0.8, 0.8, 0.7]}, padding: 0, minWidth: 0}}>
+                <img src={messages} alt="сообщения" />
+              </IconButton>
+            </Tooltip>
+          </Stack>
         </Stack>
       </Stack>
     </header>
