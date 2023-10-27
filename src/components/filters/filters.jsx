a813dc5a37897d279ace4661e1e77c3650575ba2
Nastya N <asrozova@gmail.com>
2023-10-27 20:20:30 +0200

    feat:dropdown_start

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -1,7 +1,8 @@
-import { Autocomplete, Box, FormControl, FormLabel, InputAdornment, InputLabel, OutlinedInput, Stack, TextField, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 import SearchInput from '../search-input/search-input';
 import WordsInput from '../words-input/words-input';
+import DropDown from '../drop-down/drop-down';
 
 function Filters () {
 
@@ -9,6 +10,7 @@ function Filters () {
     <Stack sx={{width: 290, outline: "1px solid red", height: 500}}>
         <Typography mb={2.5} sx={{fontFamily: 'YS Display', fontSize: '20px', fontStyle: 'normal', fontWeight: 500, lineHeight: '24px'}}>Фильтры</Typography>
         <SearchInput />
+        <DropDown />
         <Box>
             <Typography mb={1} mt={2.5} sx={{fontFamily: 'YS Display', fontSize: '16px', fontStyle: 'normal', fontWeight: 500, lineHeight: '20px'}}>Слова исключения</Typography>
             <WordsInput />
