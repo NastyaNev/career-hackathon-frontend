c899a7e23ba8ea9e7d533211c9dd300c1f5d1e7d
Nastya N <asrozova@gmail.com>
2023-10-28 14:05:30 +0200

    icons_for_candidate

--- src/pages/candidates/candidat.jsx
+++ src/pages/candidates/candidat.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import blackArrow from '../../images/blackarrow.svg';
+import stylesBack from '../vacancies/vacancy/styles.module.css';
 
 // Кандидат
 
 function Candidat() {
   return (
     <>
-        <NavLink to="/candidates">
-            <p>Назад в Все кандидаты</p>
+        <NavLink to="/candidates" className={stylesBack.link}>
+          <img src={blackArrow} alt="Стрелочка"/>
+          <p className={stylesBack.back}>Все кандидаты</p>
         </NavLink>
         <p>Привет! Я страница с кандидатом</p>
     </>
