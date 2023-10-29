4adcc0b9622f55be8bd06bffc261e943df7caa15
Nastya N <asrozova@gmail.com>
2023-10-29 02:00:55 +0100

    layout:vacancies_and_candidates_grids

--- src/pages/candidates/candidates.jsx
+++ src/pages/candidates/candidates.jsx
@@ -1,17 +1,34 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import Filters from 'components/filters/filters'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import {itemsTime} from '../../utils/arrays/items-time';
+import DirectionMainCardTime from 'components/direction-main-card/direction-main-card-time';
+import styles from './candidates.module.css'
 
 function Сandidates() {
   return (
-    <Box>
+    <>
       <Typography mb={3.5} sx={{fontАamily: 'YS Display', fontSize: '24px', fontStyle: 'normal', fontWeight: 500, lineHeight: '24px'}}>Кандидаты</Typography>
-      <Filters />
-      <NavLink to='/candidates/candidate' >
-        <Button>Кандидат</Button>
-      </NavLink>
-    </Box>
+
+      <Stack direction='row' >
+        <Box mr={2.5}>
+          <Filters />
+          <NavLink to='/candidates/candidate' >
+            <Button>Кандидат</Button>
+          </NavLink>
+        </Box>
+
+        <Grid container rowSpacing={2.5} columnSpacing={{ xs: 1, sm: 2, md: 2.75 }} mt={2.75} className={styles.grid_container}>
+          {itemsTime.map((item, i) => (
+            <Grid item>
+                <DirectionMainCardTime key={i} item={item} wayTime='/candidates/candidate' />
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
+    </>
   )
 }
 
--- src/pages/vacancies/vacancies.jsx
+++ src/pages/vacancies/vacancies.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@mui/material';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import { Outlet } from "react-router";
 
 function Vacancies() {
 
@@ -10,6 +11,7 @@ function Vacancies() {
         <NavLink to='/vacancies/vacancy' >
           <Button>Вакансия</Button>
         </NavLink>
+        <Outlet />
     </>
   )
 }
